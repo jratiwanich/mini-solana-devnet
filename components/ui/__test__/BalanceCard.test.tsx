ba507b83e1d27fa458f92cc3d21764748fb7fa28
Jak Ratiwanich <dolos.jak@gmail.com>
2025-09-13 13:02:06 -0700

    fix: unit testing

--- components/ui/__test__/BalanceCard.test.tsx
+++ components/ui/__test__/BalanceCard.test.tsx
@@ -1,6 +1,25 @@
 import { render, screen } from '@testing-library/react'
 import { BalanceCard } from '../BalanceCard'
 
+// Mock the Solana wallet adapter hooks
+jest.mock('@solana/wallet-adapter-react', () => ({
+  useConnection: () => ({
+    connection: {
+      getBalance: jest.fn(),
+      onAccountChange: jest.fn(),
+      removeAccountChangeListener: jest.fn(),
+    },
+  }),
+  useWallet: () => ({
+    publicKey: null,
+  }),
+}))
+
+// Mock Solana Web3.js
+jest.mock('@solana/web3.js', () => ({
+  LAMPORTS_PER_SOL: 1000000000,
+}))
+
 test('renders SOL Balance heading', () => {
   render(<BalanceCard />)
   expect(screen.getByText(/SOL Balance/i)).toBeInTheDocument()
--- jest.config.cjs
+++ jest.config.cjs
@@ -7,6 +7,17 @@ module.exports = {
   },
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+      },
+    }],
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@solana|@noble|@walletconnect)/)',
+  ],
+  globals: {
+    TextEncoder: require('util').TextEncoder,
+    TextDecoder: require('util').TextDecoder,
   },
 }
